90f51e91e4e0eace0dd56191eec55bb40a262458
OrghoDeb <orghodeb@gmail.com>
2024-04-05 12:54:11 +0300

    initial commit :tada:

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          MY NAME IS ORGHO
         </a>
       </header>
     </div>
